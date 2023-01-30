3feec275cff9d6740c5a9b6c46e9e0b38faa5218
chelly1995 <92913531+chelly1995@users.noreply.github.com>
2023-01-30 18:15:35 +0900

    220130

---
+++ 230130/PainterApp/moveitem.cpp
@@ -0,0 +1,15 @@
+#include "moveitem.h"
+
+MoveItem::MoveItem(QObject *parent)
+    : QObject{parent}, QGraphicsItem()
+{
+
+}
+
+
+//void MoveItem::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+//{
+//    painter
+
+
+//}
